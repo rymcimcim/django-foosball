5bda18b7fbff0de4a6f927b53c084cfa7dd67374
Jedrzej <jedrzej.jakubowski@idego.pl>
2016-10-01 13:33:59 +0200

    fix team serializer

--- game/serializers.py
+++ game/serializers.py
@@ -19,52 +19,59 @@ class TeamCreateSerializer(serializers.Serializer):
         pass
 
     def save(self, **kwargs):
-        player1 = self.validated_data['player1']
-        player2 = self.validated_data['player2']
-        player3 = self.validated_data['player3']
-        player4 = self.validated_data['player4']
+        player1_name = self.validated_data['player1']
+        player2_name = self.validated_data['player2']
+        player3_name = self.validated_data['player3']
+        player4_name = self.validated_data['player4']
 
-        # team1 = Team.objects.filter(players__name=player1_name).filter(players__name=player2_name)
-        # team2 = Team.objects.filter(players__name=player3_name).filter(players__name=player4_name)
-        # if len(team1) == 0:
-        #
-        #     player1, created = Player.objects.get_or_create(name=player1_name)
-        #     player2, created = Player.objects.get_or_create(name=player2_name)
-        #
-        #     if len(team2) == 0:
-        #         player3, created = Player.objects.get_or_create(name=player3_name)
-        #         player4, created = Player.objects.get_or_create(name=player4_name)
-        #
-        #         team2 = Team.objects.create()
-        #         team2.players.add(player3)
-        #         team2.players.add(player4)
-        #     else:
-        #         raise ValidationError('Team with those players exists.')
-        #
-        #     team1 = Team.objects.create()
-        #     team1.players.add(player1)
-        #     team1.players.add(player2)
-        #     return team1, team2
-        # raise ValidationError('Team with those players already exists.')
-        #
-        # elif player1_name and player2_name and (player3_name or player4_name):
-        #     raise ValidationError('You must specify 2 or 4 players.')
-        #
-        # elif player1_name and player2_name:
-        # if player1_name != player2_name:
-        #     team = Team.objects.filter(players__name=player1_name).filter(players__name=player2_name)
-        #     if len(team) == 0:
-        #         player1, created = Player.objects.get_or_create(name=player1_name)
-        #         player2, created = Player.objects.get_or_create(name=player2_name)
-        #         team = Team.objects.create()
-        #         team.players.add(player1)
-        #         team.players.add(player2)
-        #         return team
-        #     raise ValidationError('Team already exists.')
-        # if player1_name == player2_name:
-        #     raise ValidationError('Players must be different.')
-        # else:
-        #     raise ValidationError('You must specify at least 2 players.')
+        if player1_name and player2_name and player3_name and player4_name:
+            if player1_name in [player2_name, player3_name, player4_name] or \
+                            player2_name in [player1_name, player3_name, player4_name] or \
+                            player3_name in [player1_name, player2_name, player4_name] or \
+                            player4_name in [player1_name, player2_name, player3_name]:
+                raise ValidationError('One or more players are duplicated.')
+
+            team1 = Team.objects.filter(players__name=player1_name).filter(players__name=player2_name)
+            team2 = Team.objects.filter(players__name=player3_name).filter(players__name=player4_name)
+            if len(team1) == 0:
+
+                player1, created = Player.objects.get_or_create(name=player1_name)
+                player2, created = Player.objects.get_or_create(name=player2_name)
+
+                if len(team2) == 0:
+                    player3, created = Player.objects.get_or_create(name=player3_name)
+                    player4, created = Player.objects.get_or_create(name=player4_name)
+
+                    team2 = Team.objects.create()
+                    team2.players.add(player3)
+                    team2.players.add(player4)
+                else:
+                    raise ValidationError('Team with those players exists.')
+
+                team1 = Team.objects.create()
+                team1.players.add(player1)
+                team1.players.add(player2)
+                return team1, team2
+            raise ValidationError('Team with those players already exists.')
+
+        elif player1_name and player2_name and (player3_name or player4_name):
+            raise ValidationError('You must specify 2 or 4 players.')
+
+        elif player1_name and player2_name:
+            if player1_name != player2_name:
+                team = Team.objects.filter(players__name=player1_name).filter(players__name=player2_name)
+                if len(team) == 0:
+                    player1, created = Player.objects.get_or_create(name=player1_name)
+                    player2, created = Player.objects.get_or_create(name=player2_name)
+                    team = Team.objects.create()
+                    team.players.add(player1)
+                    team.players.add(player2)
+                    return team
+                raise ValidationError('Team already exists.')
+            if player1_name == player2_name:
+                raise ValidationError('Players must be different.')
+        else:
+            raise ValidationError('You must specify at least 2 players.')
 
 
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
