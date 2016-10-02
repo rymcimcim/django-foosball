9c63603eb09f090388e81afe5325bc9bea3cdbed
Jędrzej Jakubowski <jedrzej.jakubowski1@gmail.com>
2016-10-02 20:46:34 +0200

    change response, add parameters validation, make SlackTestView methods, add requests to requirements.txt, add players initial migration

--- game/views.py
+++ game/views.py
@@ -49,6 +49,10 @@ class MatchDetail(generics.RetrieveDestroyAPIView):
 class SlackTestView(APIView):
     def get_data(self):
         text_list = self.request.data['text'].split(' ')
+
+        if len(text_list) < 6:
+            raise ValidationError('6 parametrów jest wymaganych')
+
         players = []
         for player in text_list[:4]:
             if player == 'ja':
@@ -56,8 +60,8 @@ class SlackTestView(APIView):
             else:
                 players.append(player)
 
-        scores = [score for score in text_list[4:6]]
-        ball = text_list[len(text_list) - 1]
+        scores = text_list[5:]
+        ball = text_list[4]
 
         try:
             player1 = Player.objects.get(login=players[0])
@@ -65,12 +69,12 @@ class SlackTestView(APIView):
             player3 = Player.objects.get(login=players[2])
             player4 = Player.objects.get(login=players[3])
         except Player.DoesNotExist:
-            raise ValidationError('Jeden lub wieku graczy nie istanieje.')
+            raise ValidationError('Jeden lub wieku graczy nie istnieje.')
 
         user_added = Player.objects.get(login=self.request.data['user_name'])
         return player1, player2, player3, player4, scores, ball, user_added
 
-    def get_response(self, winner_score, looser_score, winner, looser, response_ball):
+    def get_response(self, winner_score, looser_score, winner, looser, response_ball, response_scores):
         response = {
             "username": "Adam Nawałka",
             "response_type": "in_channel",
@@ -79,7 +83,7 @@ class SlackTestView(APIView):
                     "fallback": "Required plain-text summary of the attachment.",
                     "color": "#36a64f ",
                     "pretext": "Zakończyła się rozgrywka w piłkarzyki",
-                    "title": "Mecz zakończył się wynikiem %s:%s" % (str(winner_score), str(looser_score)),
+                    "title": "Mecz zakończył się wynikiem %s:%s (%s)" % (str(winner_score), str(looser_score), response_scores),
                     "fields": [
                         {
                             "title": "Zwyciężcy",
@@ -105,6 +109,25 @@ class SlackTestView(APIView):
             response_ball = 'wolnej'
         return response_ball
 
+    def get_match_set_points(self, team1_points_list, team2_points_list):
+        if sum(team1_points_list) > sum(team2_points_list):
+            match_set_winner_points = team1_points_list
+            match_set_looser_points = team2_points_list
+        else:
+            match_set_winner_points = team2_points_list
+            match_set_looser_points = team1_points_list
+        return match_set_winner_points, match_set_looser_points
+
+    def get_team_scores(self, team1_points_list, team2_points_list):
+        team1_score = 0
+        team2_score = 0
+        for points1, points2 in zip(team1_points_list, team2_points_list):
+            if points1 > points2:
+                team1_score += 1
+            else:
+                team2_score += 1
+        return team1_score, team2_score
+
     def post(self, request, format=None):
         if 'token' not in request.data or request.data['token'] != settings.SLACK_TOKEN:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
@@ -119,22 +142,14 @@ class SlackTestView(APIView):
         team1_points = sum(team1_points_list)
         team2_points = sum(team2_points_list)
 
-        team1_score = 0
-        team2_score = 0
-        for points1, points2 in zip(team1_points_list, team2_points_list):
-            if points1 > points2:
-                team1_score += 1
-            else:
-                team2_score += 1
+        match_set_winner_points, match_set_looser_points = self.get_match_set_points(team1_points_list, team2_points_list)
+        team1_score, team2_score = self.get_team_scores(team1_points_list, team2_points_list)
 
         winner = team1 if team1_points > team2_points else team2
-
         winner_score = team1_score if team1_score > team2_score else team2_score
-        winner_points = team1_points if team1_points > team2_points else team2_points
-        looser = team1 if team1_points < team2_points else team2
 
+        looser = team1 if team1_points < team2_points else team2
         looser_score = team1_score if team1_score < team2_score else team2_score
-        looser_points = team1_points if team1_points < team2_points else team2_points
 
         match = Match.objects.create(
             added_by=user_added,
@@ -145,11 +160,15 @@ class SlackTestView(APIView):
             ball=ball
         )
 
-        match_set1 = MatchSet.objects.create(
-            match=match,
-            winner_points=winner_points,
-            looser_points=looser_points
-        )
-        data = self.get_response(winner_score, looser_score, winner, looser, self.get_ball(ball))
+        for match_set_winner, match_set_looser in zip(match_set_winner_points, match_set_looser_points):
+            MatchSet.objects.create(
+                match=match,
+                winner_points=match_set_winner,
+                looser_points=match_set_looser
+            )
+
+        response_scores = ' '.join(scores)
+
+        data = self.get_response(winner_score, looser_score, winner, looser, self.get_ball(ball), response_scores)
         r = requests.post(settings.SLACK_MATCH_WEBHOOK_URL, data=data)
         return Response(status=status.HTTP_200_OK)
