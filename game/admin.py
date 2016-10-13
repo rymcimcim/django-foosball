a1f31617face48ea585d5e947de1a57ccc429ee6
Piotr Kulas <piotr.kulas@idego.pl>
2016-10-13 12:22:57 +0200

    Improved match admin page

--- game/admin.py
+++ game/admin.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from game.models import Match
+from game.models import Match, MatchSet
+
+
+class MatchSetInline(admin.TabularInline):
+    model = MatchSet
+    extra = 0
 
 
 class MatchAdmin(admin.ModelAdmin):
+    inlines = [MatchSetInline, ]
     pass
 
 
